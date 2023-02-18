e7c82d79229cadc9ff8eb22747c74e3e81ff0cc5
Tom Cabanski <tom@cabanski.com>
2023-02-18 09:37:51 -0600

    Able to read from open5e format

---
+++ source/EncounterTracker.Open5E/Program.cs
@@ -0,0 +1,71 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Security.Cryptography.X509Certificates;
+using Autofac;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Raven.Client.Documents;
+using Raven.Client.Documents.BulkInsert;
+using Raven.Client.Documents.Session;
+using Encounter = EncounterTracker.Shared.FifthEdition.Encounter;
+
+var container = ConfigureContainer();
+
+dynamic importData;
+if (args.Length < 2)
+{
+    Console.WriteLine("Usage is: EncounterTracker.Open5e <source> <file>");
+    return;
+}
+
+Console.WriteLine($"Importing file {args[1]} from {args[0]}");
+
+using (StreamReader r = new StreamReader(args[1]))
+{
+    string json = r.ReadToEnd();
+    importData = JsonConvert.DeserializeObject(json);
+}
+
+int creatureCount = 0;
+int encounterCount = 0;
+
+using (BulkInsertOperation bulkInsert = container.Resolve<IDocumentStore>().BulkInsert())
+{
+    foreach (dynamic entry in importData)
+    {
+        EncounterTracker.Shared.Open5E.Creature importedCreature =
+                JsonConvert.DeserializeObject<EncounterTracker.Shared.Open5E.Creature>(entry.ToString());
+
+
+
+        //if (entry.Name.StartsWith("Creatures."))
+        //{
+        //    Dnd5ECreature importedCreature = JsonConvert.DeserializeObject<Dnd5ECreature>(entry.Value.ToString());
+        //    Creature creature = new EncounterTracker.Shared.FifthEdition.Creature(importedCreature);
+        //    bulkInsert.Store(creature);
+        //    creatureCount++;
+        //    Console.WriteLine($"Processed creature {creature.Id} with name {creature.Name} ");
+        //}
+
+        //if (entry.Name.StartsWith("SavedEncounters."))
+        //{
+        //    Encounter importedEncounter = JsonConvert.DeserializeObject<Encounter>(entry.Value.ToString());
+        //    var encounter = new EncounterTracker.Shared.FifthEdition.Encounter(importedEncounter);
+        //    bulkInsert.Store(encounter);
+        //    encounterCount++;
+        //    Console.WriteLine($"Processed encounter {encounter.Id} with name {encounter.Path}/{encounter.Name} ");
+
+        //}
+    }
+}
+
+Console.WriteLine($"Imported {creatureCount} creatures");
+Console.WriteLine($"Imported {encounterCount} encounters");
+
+IContainer ConfigureContainer()
+{
+    var builder = new ContainerBuilder();
+    builder.RegisterAssemblyModules(typeof(EncounterTracker.Data.Registrar).Assembly);
+
+    return builder.Build();
+}
---
+++ source/Shared/Open5E/Creature.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncounterTracker.Shared.Open5E
+{
+    public class Creature
+    {
+        public string Name { get; set; }
+        public string Size { get; set; }
+        public string Speed { get; set; }
+        public string Type { get; set; }
+        public string Alignment { get; set; }
+        public int ArmorClass { get; set; }
+        public int HitPoints { get; set; }
+        public string HitDice { get; set; }
+        public int Strength { get; set; }
+        public int Dexterity { get; set; }
+        public int Constitution { get; set; }
+        public int Intelligence { get; set; }
+        public int Wisdom { get; set; }
+        public int Charisma { get; set; }
+        public int? Constitution_save { get; set; }
+        public int? Intelligence_save { get; set; }
+        public int? Wisdom_save { get; set; }
+        public int? Dexterity_save { get; set; }
+        public int? Charisma_save { get; set; }
+        public int? Strength_save { get; set; }
+        public int? Acrobatics { get; set; }
+        public int? Arcana { get; set; }
+        public int? Athletics { get; set; }
+        public int? Deception { get; set; }
+        public int? History { get; set; }
+        public int? Insight { get; set; }
+        public int? Intimidation { get; set; }
+        public int? Investigation { get; set; }
+        public int? Medicine { get; set; }
+        public int? Nature { get; set; }
+        public int? Perception { get; set; }
+        public int? Performance { get; set; }
+        public int? Persuasion { get; set; }
+        public int? Religion { get; set; }
+        public int? Stealth { get; set; }
+        public int? Survival { get; set; }
+        public string Damage_vulnerabilities { get; set; }
+        public string Damage_resistances { get; set; }
+        public string Damage_immunities { get; set; }
+        public string Condition_immunities { get; set; }
+        public string Senses { get; set; }
+        public string Languages { get; set; }
+        public int Challenge_rating { get; set; }
+        public List<Ability> Special_abilities { get; set; }
+        public List<Ability> Actions { get; set; }
+        public List<Ability> Legendary_actions { get; set; }
+    }
+
+    public struct Ability
+    {
+        public string Name { get; set; }
+        public string Desc { get; set; }
+        
+    }
+}
